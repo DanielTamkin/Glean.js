702a72269a5df7d81f87b698afc17ac557da81ee
danieltamkin <dudearoo@gmail.com>
2015-10-15 14:51:32 -0500

    Work on html() functions

--- Glean.js
+++ Glean.js
@@ -8,23 +8,25 @@
 	----
 	output: http://danieltamkin.com
 */
-$.fn.ShopDown = function(data) {
+$.fn.Glean = function(data) {
 	$this = $(this);
 	$data = [];
 	$data.settings = datacheck(data);
+	$data.html = {
+		sanitizeTo: $data.settings.sanitizeTo,
+		get: false,
+		taken: null
+	}
 	/*data*/
 	function datacheck(data){
 	  return datascrub(data);
 	}
 	function datatemplate(){
 	  return {
-			leftovers: false,
 	    content: $this,
 	    sanitize: true,
-			cull: true,
-			images: true,
-			compile: true
-	  }
+			sanitizeTo: "#glean-sanitized"
+		}
 	}
 	function datascrub(data){
 	  template = datatemplate();
@@ -52,17 +54,62 @@ $.fn.ShopDown = function(data) {
 		}
 		return data;
 	}
+/*> syntax*/
+	function syntaxGather(){}
+	function syntaxApprove(){}
+/*> html*/
+	function html(){
+		if($data.html.get != true){
+			var html = $($data.settings.content).html();
+		}
+	}
+	function htmlGet(){}
+	function htmlSiftTo(id){
+		data = {
+			h1: 'h1#'+id,
+			h2: 'h2#'+id,
+			h3: 'h3#'+id,
+			h4: 'h4#'+id,
+			h5: 'h5#'+id,
+			h6: 'h6#'+id
+		}
+		var	content		= html(),
+				SiftedTo 	= null;
+		$.each(data,function(key,value){
+			Sift = content.find(value);
+			if(Sift.length < 1){
+				// continue to find the proper value, this one doesn't exist.
+			}
+			else{
+				SiftedTo = value;
+				return false;// break out of loop
+			}
+		});
+		return SiftedTo;
+	}
+	function htmlSanitize(){
+		if($data.settings.sanitize != true){
+			return null;// dont do anything.
+		}
+		else{
+
+		}
+	}
 /*> app*/
 	function app(){
-	  window.htmlGot = false;// yes your allowed to grab HTML!
 		appCalculation();
-		appCleanup();
 	}
+	function appCalculation(){
+
+	}
+	function appGet(){
+		return $data.settings;
+	}
+/*> objective functions / Runnables*/
 	app();
-/*> objective functions*/
 	return {
-	  output: function(data) {
-	    return appOutput(data);
+	  get: function(data) {
+	    return appGet(data);
 	  },
 		reload: function(){
 			app();// just reloads the app model to get new info.

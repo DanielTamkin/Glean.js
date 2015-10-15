b0c85cbeb6cd0d7e37ba74727c2dcd0a9b68c41e
danieltamkin <dudearoo@gmail.com>
2015-10-15 13:52:23 -0500

    Created Glean, the product from the generic data tests.

---
+++ Glean.js
@@ -0,0 +1,10 @@
+/*
+	Glean.js - the final product from the ShopDown.js tests.
+	proposed use.
+  ----
+  	var Glean 	= $("thiselement.section").Glean(),
+  			link 		= Glean.get("link");
+  	console.log(link);
+	----
+	output: http://danieltamkin.com
+*/

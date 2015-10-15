bc048c7ef91886a3ee1ba83154a4851a638e2d28
danieltamkin <dudearoo@gmail.com>
2015-10-15 14:05:43 -0500

    Added basic structure

--- Glean.js
+++ Glean.js
@@ -8,3 +8,64 @@
 	----
 	output: http://danieltamkin.com
 */
+$.fn.ShopDown = function(data) {
+	$this = $(this);
+	$data = [];
+	$data.settings = datacheck(data);
+	/*data*/
+	function datacheck(data){
+	  return datascrub(data);
+	}
+	function datatemplate(){
+	  return {
+			leftovers: false,
+	    content: $this,
+	    sanitize: true,
+			cull: true,
+			images: true,
+			compile: true
+	  }
+	}
+	function datascrub(data){
+	  template = datatemplate();
+		if(data == undefined || $.isEmptyObject(data) == true){
+			data = template;
+		}
+		else{
+		  $.each(template,function(key,value){
+		    $.each(data,function(bravokey,bravovalue){
+		      if(bravokey == key){
+		        if(bravovalue.length === -1){
+		          value = value;
+		        }
+		        else{
+		          // do nothing, bravovalue is in bravokey
+		        }
+		      }
+		      else{
+		        if(!(key in data)){// that key doesnt exist? make it.
+		          data[key] = value;
+		        }
+		      }
+		    });
+		  });
+		}
+		return data;
+	}
+/*> app*/
+	function app(){
+	  window.htmlGot = false;// yes your allowed to grab HTML!
+		appCalculation();
+		appCleanup();
+	}
+	app();
+/*> objective functions*/
+	return {
+	  output: function(data) {
+	    return appOutput(data);
+	  },
+		reload: function(){
+			app();// just reloads the app model to get new info.
+		}
+	}
+};

f02a3955c8a70bc0fec5dfb7ad75087a70d0c3a6
Daniel Tamkin <dudearoo@gmail.com>
2017-05-01 19:58:17 -0500

    Updated to support promises and proper callbacks

--- Glean.js
+++ Glean.js
@@ -1,193 +1,231 @@
-/*
-	Glean.js - the final product from the ShopDown.js tests.
-	proposed use.
-  ----
-  	var Glean 	= $("thiselement.section").Glean(),
-  			link 		= Glean.get("link");
-  	console.log(link);
-	----
-	output: http://danieltamkin.com
-*/
-$.fn.Glean = function(data) {
-	$this = $(this);
-	$data = [];
-	$data.settings = datacheck(data);
-	$data.html = {
-		variablesGet: $data.settings.variablesGet,
-		get: false,
-		taken: null
-	}
-	$data.syntax = {
-		opening: "!/",
-		closing: "/!"
-	}
-/*> data*/
-	function datacheck(data){
-	  return datascrub(data);
-	}
-	function datatemplate(){
-	  return {
-	    content: $this,
-			variablesGet: "glean-get"
-		}
-	}
-	function datascrub(data){
-	  template = datatemplate();
-		if(data == undefined || $.isEmptyObject(data) == true){
-			data = template;
-		}
-		else{
-		  $.each(template,function(key,value){
-		    $.each(data,function(bravokey,bravovalue){
-		      if(bravokey == key){
-		        if(bravovalue.length === -1){
-		          value = value;
-		        }
-		        else{
-		          // do nothing, bravovalue is in bravokey
-		        }
-		      }
-		      else{
-		        if(!(key in data)){// that key doesnt exist? make it.
-		          data[key] = value;
-		        }
-		      }
-		    });
-		  });
-		}
-		return data;
-	}
-/*> syntax*/
-	function syntaxGather(){
-		var possible 	= syntaxPossible(); // tags and approves all ID's
-		if(possible == true){
-			$(".glean-gather").each(function(i,node){
-				$this.find("#"+$data.settings.variablesGet).append($(this));
-			});
-		}
-		else{
-			appWarn("no variables detected.");
-			// dont do anything. You cant, no variables.
-		}
-	}
-	function syntaxPossible(){
-		$this.find("h1, h2, h3, h4, h5, h6").addClass("glean-possible");
-		var possible = 0;
-		$this.find('.glean-possible').each(function(i, node) {
-			var approv = syntaxApprove($(this).text());
-			if(approv == 100){
-				$(this).removeClass("glean-possible");
-				$(this).addClass("glean-gather");
-				$(this).next().addClass("glean-gather");
-				possible++;
-			}
-			else if(approv == 206){
-				$(this).removeClass("glean-possible");
-				appWarn("Syntax error, removing declaration");
-				// warn and remove class + the tag that still exits.
-			}
-			else{
-				$(this).removeClass("glean-possible");
-			}
-		});
-		if(possible != 0){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	function syntaxApprove(content){
-		if(content.indexOf($data.syntax.opening) == 0){
-			if(content.lastIndexOf($data.syntax.closing) == content.length - $data.syntax.closing.length){
-				return 100;// all good pass
-			}
-			else{
-				return 206;// errors in the syntax
-			}
-		}
-		else{
-			if(content.lastIndexOf($data.syntax.closing) == content.length - $data.syntax.closing.length){
-				return 206;// errors in the syntax
-			}
-			else{
-				return 406;// no syntax detected
-			}
-		}
-	}
-/*> html*/
-	function htmlFind(variable){
-		var content = $this.find("#"+$data.html.variablesGet),
-				element = content.find("#"+variable),
-				data 		=  element.next().html();
-		if(data == undefined){
-			return undefined;
-		}
-		else{
-			return data.trim();// removes whitespaces
-		}
-	}
-	function htmlAppend(){
-		$this.append("<div id=\""+$data.html.variablesGet+"\" style=\"display:none;\"></div>");
-	}
-	function htmlIdCheck(){
-		var	content		= $($data.settings.content).html();
-		$this.find("h1, h2, h3, h4, h5, h6").addClass("glean-idcheck");
-		$this.find('.glean-idcheck').each(function(i, node) {
-			var data 			= $(this).text(),
-					data 			= Idify(data),
-					dataFront = data.slice(0,1),
-					dataTail  = data.slice(data.length-1,data.length),
-					id 				= $(this).attr("id");
-			if(data != id){
-				$(this).removeAttr("id");
-				$(this).attr("id",data);// re inits the new id
-			}
-			else{
-				// leave it its all good.
-			}
-		});
-		$this.find("h1, h2, h3, h4, h5, h6").removeClass("glean-idcheck");
-	}
-/*> Idify*/
-	function Idify(variable){
-		return variable.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '').toLowerCase();
-	}
-/*> app*/
-	function app(){
-		appCalculation();
-	}
-	function appCalculation(){
-		htmlIdCheck();
-		htmlAppend();
-		syntaxGather();
-	}
-	function appWarn(message){
-		console.warn("Glean: WARN - "+message);
-	}
-	function appError(message){
-		console.error("Glean: ERROR - "+message);
-	}
-	function appLog(message){
-		console.log("Glean: "+message);
-	}
-	function appGet(variable){
-		if(variable != undefined){
-			variable = Idify(variable);
-			return htmlFind(variable);
-		}
-		else{
-			return $data.settings;
-		}
-	}
-/*> objective functions / Runnables*/
-	app();
-	return {
-	  get: function(variable) {
-	    return appGet(variable);
-	  },
-		reload: function(){
-			app();// just reloads the app model to get new info.
-		}
-	}
-};
+(function($){
+
+  $.fn.Glean = function(options){
+    var defaults = {
+      content: this,
+      html: {
+        get: "glean-get",
+        find: "h1, h2, h3, h4, h5, h6",
+        gather: "glean-gather"
+      },
+      syntax: {
+        opening: "!/",
+        closing: "/!"
+      },
+      onStart: function(){},
+      onDone: function(){}
+    };
+    $settings   = $.extend(true,{},defaults , options);
+    $variables  = {};
+    $this       = this;
+    /*
+     Public functions
+    */
+    this.build = function(){
+      var dfd = jQuery.Deferred();
+      $settings.onStart();
+      $.when(
+        htmlAppend(),
+        htmlIdCheck(),
+        syntaxGather(),
+        varsLoad()
+      ).done(function(){
+        $settings.onDone($variables);
+        dfd.resolve($variables);
+      })
+      .fail(function(){
+        dfd.reject();
+      });
+      return dfd.promise();
+    };
+    /**
+     * Get a variable specified.
+     * @param  {[String]}   [variables title/key]
+     * @return {[type]}     [The variables value]
+     */
+    this.get = function(variable){
+      if(variable != undefined){
+  			return htmlFind(htmlSlugify(variable));
+  		}
+  		else{
+  			return undefined;
+  		}
+    }
+    /*
+     Private functions
+    */
+
+    /**
+     * Checks that each $settings.html.find has an id that
+     * is a slugged version of its data, if it does not then make it
+     * have one.
+     * @param  {Function} callback [called at end of function]
+     */
+    function htmlIdCheck(){
+      var dfd = jQuery.Deferred();
+      var	content		= $(this).html();
+      $this.find($settings.html.find).each(function(i, node) {
+        var data 			= htmlSlugify($(this).text()),
+            id 				= $(this).attr("id");
+        if(data != id){
+          $(this).removeAttr("id");
+          $(this).attr("id",data);// re inits the new id
+        }
+        else{
+          // has id already
+        }
+      });
+      dfd.resolve();
+      return dfd.promise();
+    }
+    /**
+     * Slugs whatever variable is handed to it.
+     * @param  {[String]} variable [Hand this string]
+     * @return {[type]}          [A slugged version of the @param]
+     */
+    function htmlSlugify(variable){
+      return variable.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
+    }
+    /*
+    * Appends a stashed version of the html that we will touch,
+    * we do this so we can scrub the visible html of any variables immediatly.
+     */
+    function htmlAppend(){
+      var dfd = jQuery.Deferred();
+      $this.append("<div id=\""+$settings.html.get+"\" style=\"display:none;\"></div>");
+      dfd.resolve();
+      return dfd.promise();
+    }
+    /**
+     * Find and return the value of a variable
+     * @param  {[String]} variable    [the slugged variable to be searched]
+     * @return {[type]}   data.trim() [the variables value]
+     */
+    function htmlFind(variable){
+  		var content =  $this.find("#"+$settings.html.get),
+  				element =  content.find("#"+variable),
+  				data 		=  element.next().hasClass($settings.html.gather).html();
+  		if(data == undefined){
+  			return undefined;
+  		}
+  		else{
+  			return data.trim();// removes whitespaces
+  		}
+  	}
+    // variables
+    function varsLoad(){
+      var dfd = jQuery.Deferred();
+      $(".glean-gather").each(function(i,node){
+        if($(this).hasClass('glean-key')){
+          var key   = varsSlugify($(this).text());
+          $variables[key] = $(this).next().html();
+        }
+      });
+      dfd.resolve();
+      return dfd.promise();
+    }
+    /**
+     * Slugs whatever variable is handed to it.
+     * @param  {[String]} variable [Hand this string]
+     * @return {[type]}          [A slugged version of the @param]
+     */
+    function varsSlugify(variable){
+      return variable.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '').toLowerCase();
+    }
+    // syntax
+
+    /**
+     * Gathers all Validated variables and shoots them to a hidden
+     * div for future use.
+     * @return {[Promise]}          [returns promise for procedure list]
+     */
+    function syntaxGather(){
+      var dfd = jQuery.Deferred();
+  		syntaxPossible(function(valid){
+        if(valid){
+    			$(".glean-gather").each(function(i,node){
+    				$this.find("#"+$settings.html.get).append($(this));//populate
+    			});
+          dfd.resolve();
+    		}
+    		else{
+    			// dont do anything. You cant, no variables.
+    			dfd.reject("no variables");
+    		}
+        return dfd.promise();
+      });
+  	}
+    /**
+     * tags all variables that are valid with the Class 'glean-gather'
+     * @param  {[callback]} function [ran with either true or false on the
+     *                               possibilities]
+     * @callback {[true/false]} [true if there is syntax, false if there is not.]
+     */
+  	function syntaxPossible(callback){
+  		var possibilities = false;
+  		$this.find($settings.html.find).each(function(i, node) {
+        var syntax = $(node).text();
+        syntaxValidate(syntax,function(valid){
+          if(valid==true){
+            $(node).removeClass("glean-possible");
+    				$(node).addClass($settings.html.gather).addClass('glean-key');
+    				$(node).next().addClass($settings.html.gather).addClass('glean-value');//capture that <p> tag
+    				possibilities = true;
+    			}
+    			else{
+    				// $(this).removeClass("glean-possible");
+    				// warn and remove class + the tag that still exits.
+    			}
+
+        });
+  		});
+      callback(possibilities);
+  	}
+
+    /**
+     * Validates the syntax of a $settings.html.find by
+     * determining that it has the intended opening syntax($settings.syntax.opening)
+     * and closing syntax($settings.syntax.closing)
+     * @param  {[syntax]}   string   [the syntax in question]
+     * @param  {[callback]} variable [Hand this string]
+     * @callback {[true/false]}      [true if valid, false if not]
+     */
+    function syntaxValidate(syntax,callback){
+  		if(syntax.indexOf($settings.syntax.opening) == 0){
+        // has both opening and closing braces
+  			if(syntax.lastIndexOf($settings.syntax.closing) == syntax.length - $settings.syntax.closing.length){
+  				callback(true);
+  			}
+  			else{
+  				callback(false);// errors in the syntax
+  			}
+  		}
+  		else{
+        // has closing brace but no opening brace
+  			if(syntax.lastIndexOf($settings.syntax.closing) == syntax.length - $settings.syntax.closing.length){
+  				callback(true)// errors in the syntax
+  			}
+  			else{
+  				callback(false)// no syntax detected
+  			}
+  		}
+  	}
+    /**
+     * [callbackMessage description]
+     * @param  {[type]} message
+     * @param  {[type]} boolean
+     * @return {[type]}         [description]
+     */
+    function callbackMessage(message,boolean){
+      return {
+        message: message,
+        status: boolean
+      }
+    }
+
+    // Compile
+    return $this.each(function() {
+      $this.build();
+    });
+  };
+}(jQuery));
